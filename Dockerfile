cc17a2ed9095ea431b2a405efbd1751af08934eb
Rafael Zamora <rafael.zamora@thehubs.company>
2024-06-06 15:28:00 +0200

    Test 4

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM scratch
+RUN docker run --privileged --rm tonistiigi/binfmt --install all
 RUN chmod +x /run.sh
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --privileged --rm tonistiigi/binfmt --install all -v tolgee_data:/data/ -p 8085:8080 tolgee/tolgee
+docker run -v tolgee_data:/data/ -p 8085:8080 tolgee/tolgee
